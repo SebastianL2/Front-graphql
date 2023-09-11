1d8c36aca6893dc987fd891cbbc004e4dcc06718
SebastianL2 <sebas16cely@gmail.com>
2023-09-11 01:34:37 -0500

    front update 4

--- src/App.js
+++ src/App.js
@@ -3,25 +3,34 @@ import React, { useState } from 'react';
 import TaskForm from './components/TasksForm';
 
 const App = () => {
-
   const [showTaskForm, setShowTaskForm] = useState(false);
 
   const toggleTaskForm = () => {
     setShowTaskForm(!showTaskForm);
   };
 
+  const closeTaskForm = () => {
+    setShowTaskForm(false);
+  };
+
   return (
-    <div>
-       <h1>Tareas</h1>
-      <button
-        className="btn btn-primary mb-3"
-        onClick={toggleTaskForm}
-      >
-        Tarea Nueva
-      </button>
-      {showTaskForm && <TaskForm />}
-    </div>
+    <div className="container text-center">
+    <h1>Tareas</h1>
+    <button
+      className="btn btn-primary mb-3"
+      onClick={toggleTaskForm}
+    >
+      Tarea Nueva
+    </button>
+    {showTaskForm && (
+      <div className="d-flex justify-content-center align-items-center ">
+        {/* Centra el TaskForm vertical y horizontalmente */}
+        <TaskForm onClose={closeTaskForm} />
+      </div>
+    )}
+  </div>
   );
 };
+
 
 export default App;
--- src/components/TasksForm.jsx
+++ src/components/TasksForm.jsx
@@ -11,7 +11,7 @@ const CREATE_TASK = gql`
   }
 `;
 
-const TaskForm = () => {
+const TaskForm = ({ onClose }) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
@@ -26,42 +26,60 @@ const TaskForm = () => {
       });
 
       console.log('Nueva tarea creada:', data.createTask);
+
+      // Limpiar el formulario después de la creación exitosa
+      setTitle('');
+      setDescription('');
+
+      // Cerrar el formulario
+      onClose();
     } catch (error) {
       console.error('Error al crear tarea:', error);
     }
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
-    <div className="mb-3">
-      <label htmlFor="title" className="form-label">
-        Título:
-      </label>
-      <input
-        type="text"
-        id="title"
-        className="form-control"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        required
-      />
+    <div className="container d-flex justify-content-center align-items-center min-vh-1">
+      {/* Contenedor centrado vertical y horizontalmente */}
+      <form onSubmit={handleFormSubmit} className="w-50">
+        {/* Contenido del formulario */}
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Título:
+          </label>
+          <input
+            type="text"
+            id="title"
+            className="form-control"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Descripción:
+          </label>
+          <textarea
+            id="description"
+            className="form-control"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Crear Tarea
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary ml-2"
+          onClick={onClose}
+        >
+          Cancelar
+        </button>
+      </form>
     </div>
-    <div className="mb-3">
-      <label htmlFor="description" className="form-label">
-        Descripción:
-      </label>
-      <textarea
-        id="description"
-        className="form-control"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        required
-      />
-    </div>
-    <button type="submit" className="btn btn-primary">
-      Crear Tarea
-    </button>
-  </form>
   );
 };
 
