eab1bbbc0a8da6d47adc62e3634c289eeb28e0e9
SebastianL2 <sebas16cely@gmail.com>
2023-09-11 02:55:34 -0500

    front update 7

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,51 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ALL_TASKS } from '../queries/queries'; // Importa la consulta
-import { DELETE_TASK } from '../queries/mutations'; // Importa la mutación
+import { UPDATE_TASK, DELETE_TASK } from '../queries/mutations'; // Importa la mutación
 
 const TaskList = () => {
-  const { loading, error, data } = useQuery(GET_ALL_TASKS);
-  const [deleteTask] = useMutation(DELETE_TASK);
-
-  const handleDeleteTask = async (taskId) => {
-    try {
-      await deleteTask({ variables: { id: taskId } });
-      alert('tarea elimminada correctamente')
-    } catch (error) {
-      alert('Error al eliminar la tarea:', error);
-    }
-  };
-
-  if (loading) return <p>Cargando...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
-  return (
-    <div>
-      <h2>Lista de Tareas</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Título</th>
-            <th>Descripción</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.getAllTasks.map((task) => (
-            <tr key={task.id}>
-              <td>{task.title}</td>
-              <td>{task.description}</td>
-              <td>
-                <button onClick={() => handleDeleteTask(task.id)} className="btn btn-danger">
-                  Eliminar
-                </button>
-              </td>
+    const { loading, error, data } = useQuery(GET_ALL_TASKS);
+    const [updateTask] = useMutation(UPDATE_TASK);
+    const [deleteTask] = useMutation(DELETE_TASK);
+    const [editedTask, setEditedTask] = useState({ id: '', title: '', description: '' });
+  
+    const handleEditTask = (task) => {
+      setEditedTask(task);
+    };
+  
+    const handleUpdateTask = async () => {
+      try {
+        const { id, title, description } = editedTask;
+        await updateTask({ variables: { id, task: { title, description } } });
+        setEditedTask({ id: '', title: '', description: '' });
+      } catch (error) {
+        console.error('Error al actualizar la tarea:', error);
+      }
+    };
+  
+    const handleDeleteTask = async (taskId) => {
+      try {
+        await deleteTask({ variables: { id: taskId } });
+      } catch (error) {
+        console.error('Error al eliminar la tarea:', error);
+      }
+    };
+  
+    if (loading) return <p>Cargando...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+  
+    return (
+      <div>
+        <h2>Lista de Tareas</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Título</th>
+              <th>Descripción</th>
+              <th>Acciones</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-export default TaskList;
+          </thead>
+          <tbody>
+            {data.getAllTasks.map((task) => (
+              <tr key={task.id}>
+                <td>
+                  {editedTask.id === task.id ? (
+                    <input
+                      type="text"
+                      value={editedTask.title}
+                      onChange={(e) => setEditedTask({ ...editedTask, title: e.target.value })}
+                    />
+                  ) : (
+                    task.title
+                  )}
+                </td>
+                <td>
+                  {editedTask.id === task.id ? (
+                    <input
+                      type="text"
+                      value={editedTask.description}
+                      onChange={(e) => setEditedTask({ ...editedTask, description: e.target.value })}
+                    />
+                  ) : (
+                    task.description
+                  )}
+                </td>
+                <td>
+                  {editedTask.id === task.id ? (
+                    <button onClick={handleUpdateTask} className="btn btn-success">
+                      Guardar
+                    </button>
+                  ) : (
+                    <>
+                      <button onClick={() => handleEditTask(task)} className="btn btn-primary mr-2">
+                        Editar
+                      </button>
+                      <button onClick={() => handleDeleteTask(task.id)} className="btn btn-danger">
+                        Eliminar
+                      </button>
+                    </>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+  
+  export default TaskList;
--- src/queries/mutations.js
+++ src/queries/mutations.js
@@ -5,3 +5,12 @@ export const DELETE_TASK = gql`
     deleteTask(id: $id)
   }
 `;
+export const UPDATE_TASK = gql`
+  mutation UpdateTask($id: ID!, $task: TaskInput!) {
+    updateTask(id: $id, task: $task) {
+      id
+      title
+      description
+    }
+  }
+  `;
